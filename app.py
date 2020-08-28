7457fb280a0137340a0da4712b5b34b96666c3ab
jesssullivan <Jess@sulliwood.org>
2020-08-28 19:46:56 -0400

    flummoxed

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask
-
-# production @ heroku:
-hostname = '0.0.0.0'
-hostport = 80
-
-# if you are using a proxy via npm, use port 5000:
-#hostname = '127.0.0.1'
-#hostport = 5000
-
 app = Flask(__name__, static_folder='./demos/')
-
 
 @app.route('/')
 def home():
     return app.send_static_file('spec_record_crop.html')
 
-
 if __name__ == '__main__':
-    app.run(host=hostname, debug=False, port=hostport)
+    flask_app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
