3342a6682a27cce177d129d8e988244b0bd06124
jesssullivan <Jess@sulliwood.org>
2020-08-28 19:20:39 -0400

    errr

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask
-import os
-
-""" Paths: """
 
 # production @ heroku:
 hostname = '0.0.0.0'
 hostport = 80
 
 # if you are using a proxy via npm, use port 5000:
-# hostname = '127.0.0.1'
-# hostport = 5000
+hostname = '127.0.0.1'
+hostport = 5000
 
 app = Flask(__name__, static_folder='./demos/')
-
-
-""" Routing: """
 
 
 @app.route('/')
@@ -23,4 +17,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host=hostname, debug=False, port=os.environ.get('PORT', 80))
+    app.run(host=hostname, debug=False, port=hostport)
