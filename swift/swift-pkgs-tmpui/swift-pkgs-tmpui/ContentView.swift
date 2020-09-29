f70bf159b8702e9214a201cdab998157aa73a910
Jess Sullivan <jess@sulliwood.org>
2020-09-29 10:57:43 -0400

    + ignore xc directory while fidding

---
+++ swift/swift-pkgs-tmpui/swift-pkgs-tmpui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-pkgs-tmpui
+//
+//  Created by Jess on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
