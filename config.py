59504153487dcc3c4793c2991eeb7b6881933df2
Jess Sullivan <jess@sulliwood.org>
2020-10-16 16:50:26 -0400

    production --> heroku

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ devhost = '127.0.0.1'
 
 # rendered html extension:
 ext = '_render.html'
-static = "./demos/"
+static = "./production/"
 
 # default html chunks:
 header = static + 'templates/header.html'
